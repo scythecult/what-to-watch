e04a9a4e8e48d2355daf33acd8611be09f66bd3f
scythecult <scythecult@gmail.com>
2023-09-30 17:49:20 +0300

    -разделяет компонент main на более мелкие компоненты WIP

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+import { Logo } from '../logo';
+
+const Footer = () => (
+  <footer className="page-footer">
+    <Logo light />
+
+    <div className="copyright">
+      <p>© 2019 What to watch Ltd.</p>
+    </div>
+  </footer>
+);
+
+export { Footer };
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,8 @@
+import { Logo } from '../logo';
+
 const Header = () => (
   <header className="page-header film-card__head">
-    <div className="logo">
-      <a className="logo__link">
-        <span className="logo__letter logo__letter--1">W</span>
-        <span className="logo__letter logo__letter--2">T</span>
-        <span className="logo__letter logo__letter--3">W</span>
-      </a>
-    </div>
+    <Logo />
 
     <ul className="user-block">
       <li className="user-block__item">
