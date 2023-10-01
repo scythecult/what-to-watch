604175c49fe6c78c1a0b46a78cce038d163c7b07
scythecult <scythecult@gmail.com>
2023-10-01 19:04:10 +0300

    -рефакторит компонент genres-filter -добавляет динамическое вычисление жанров на основе входящих данных

--- src/components/genres-filter/genres-filter.tsx
+++ src/components/genres-filter/genres-filter.tsx
@@ -1,56 +1,21 @@
-const GenresFilter = () => (
-  <ul className="catalog__genres-list">
-    <li className="catalog__genres-item catalog__genres-item--active">
-      <a href="#" className="catalog__genres-link">
-        All genres
-      </a>
-    </li>
-    <li className="catalog__genres-item">
-      <a href="#" className="catalog__genres-link">
-        Comedies
-      </a>
-    </li>
-    <li className="catalog__genres-item">
-      <a href="#" className="catalog__genres-link">
-        Crime
-      </a>
-    </li>
-    <li className="catalog__genres-item">
-      <a href="#" className="catalog__genres-link">
-        Documentary
-      </a>
-    </li>
-    <li className="catalog__genres-item">
-      <a href="#" className="catalog__genres-link">
-        Dramas
-      </a>
-    </li>
-    <li className="catalog__genres-item">
-      <a href="#" className="catalog__genres-link">
-        Horror
-      </a>
-    </li>
-    <li className="catalog__genres-item">
-      <a href="#" className="catalog__genres-link">
-        Kids & Family
-      </a>
-    </li>
-    <li className="catalog__genres-item">
-      <a href="#" className="catalog__genres-link">
-        Romance
-      </a>
-    </li>
-    <li className="catalog__genres-item">
-      <a href="#" className="catalog__genres-link">
-        Sci-Fi
-      </a>
-    </li>
-    <li className="catalog__genres-item">
-      <a href="#" className="catalog__genres-link">
-        Thrillers
-      </a>
-    </li>
-  </ul>
-);
+import { mockFilms } from '../../mocks/films';
+import { getGenres } from '../../utils';
+
+// catalog__genres-item--active
+const GenresFilter = () => {
+  const genres = getGenres(mockFilms);
+
+  return (
+    <ul className="catalog__genres-list">
+      {genres.map((genre) => (
+        <li className="catalog__genres-item" key={genre}>
+          <a href="#" className="catalog__genres-link">
+            {genre}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export { GenresFilter };
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,17 @@
+import { TFilm } from './types';
+
+const getGenres = (films: TFilm[]) =>
+  Object.keys(
+    films.reduce(
+      (initial, mockFilm) => ({ ...initial, [mockFilm.genre]: '' }),
+      { 'All Genres': '' }
+    )
+  );
+
 const generateRatingValues = (count: number) =>
   Array.from({ length: count })
     .fill('')
     .map((_, index) => index + 1)
     .reverse();
 
-export { generateRatingValues };
+export { generateRatingValues, getGenres };
