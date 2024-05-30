178170b4af5bdac2f131f616244a86ca7edfc61e
scythecult <scythecult@gmail.com>
2024-05-30 14:28:58 +0300

    [module7-task1] RELOC-COMMIT

--- src/store/filter-slice/filter-slice.test.ts
+++ src/store/filter-slice/filter-slice.test.ts
@@ -1,6 +1,6 @@
 import { StoreName } from '../const';
 import { StoreType, getStore } from '../store';
-import { filterSlice } from './filter-slice';
+import { filterSlice, setFilter } from './filter-slice';
 
 describe('FiltersSlice selectors', () => {
   let store: StoreType;
@@ -17,5 +17,15 @@ describe('FiltersSlice selectors', () => {
     const result = filterSlice.reducer(undefined, action);
 
     expect(result).toEqual(initialState);
+  });
+
+  it('Should set currentFilter', () => {
+    const state = store.getState();
+    const initialState = state[StoreName.Filters];
+    const currentFilter = 'Check';
+
+    const result = filterSlice.reducer(initialState, setFilter(currentFilter));
+
+    expect(result.currentFilter).toBe(currentFilter);
   });
 });
